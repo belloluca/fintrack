6c59d1d4faa47c8e44a3836d42539de075f34076
Bello Luca <belloluca2002@gmail.com>
2026-09-02 22:42:05 +0200

    page's header added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })

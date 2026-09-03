51d5be9889e248f68321f91d2ec932f4b6ff08b4
Bello Luca <belloluca2002@gmail.com>
2026-09-03 20:16:34 +0200

    add typed transaction

--- src/app/services/transaction-service.spec.ts
+++ src/app/services/transaction-service.spec.ts
@@ -1,15 +1,49 @@
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { TransactionService } from './transaction-service';
+
+import type { Transaction } from '../models/transaction-model';
+import { TransactionService } from './transactionApi';
 
 describe('TransactionService', () => {
   let service: TransactionService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [TransactionService, provideHttpClient(), provideHttpClientTesting()],
+    });
+
     service = TestBed.inject(TransactionService);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTesting.verify();
+  });
+
+  it('should retrieve all transactions', () => {
+    const mockTransactions: Transaction[] = [
+      {
+        id: 1,
+        description: 'Stipendio',
+        amount: 1800,
+        type: 'INCOME',
+        categoryId: 1,
+        date: '2026-09-01',
+        createdAt: '2026-09-01T08:00:00.000Z',
+        updatedAt: '2026-09-01T08:00:00.000Z',
+      },
+    ];
+
+    service.getTransactions().subscribe((transactions) => {
+      expect(transactions).toEqual(mockTransactions);
+    });
+
+    const request = httpTesting.expectOne('/api/transactions');
+
+    expect(request.request.method).toBe('GET');
+
+    request.flush(mockTransactions);
   });
 });
--- src/app/services/transactionApi.ts
+++ src/app/services/transactionApi.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Transaction } from '../models/transaction-model';
 import { Observable } from 'rxjs';
+import { CreateTransactionRequest, UpdateTransactionRequest } from '../models/request-type';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +18,17 @@ export class TransactionService {
 
   getTransactionById(id: number): Observable<Transaction> {
     return this.http.get<Transaction>(`${this.apiUrl}/${id}`);
+  }
+
+  createTransaction(data: CreateTransactionRequest): Observable<Transaction> {
+    return this.http.post<Transaction>(this.apiUrl, data);
+  }
+
+  updateTransaction(id: number, data: UpdateTransactionRequest): Observable<Transaction> {
+    return this.http.put<Transaction>(`${this.apiUrl}/${id}`, data);
+  }
+
+  deleteTransaction(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }

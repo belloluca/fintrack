99c1e402e0b121fecd1925ab9925dd7bffb71096
Bello Luca <belloluca2002@gmail.com>
2026-09-04 16:51:47 +0200

    Add transaction loading with loading/error state handling

--- src/app/features/transactions/transaction-list-page/transaction-list-page.ts
+++ src/app/features/transactions/transaction-list-page/transaction-list-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { TransactionService } from '../../../services/transactionApi';
+import { Transaction } from '../../../models/transaction-model';
+import { finalize } from 'rxjs';
 
 @Component({
   imports: [],
@@ -6,4 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './transaction-list-page.scss',
   templateUrl: './transaction-list-page.html',
 })
-export class TransactionListPage {}
+export class TransactionListPage implements OnInit {
+  private readonly transactionService = inject(TransactionService);
+
+  protected readonly data = signal<Transaction[]>([]);
+
+  protected readonly loading = signal(true);
+  protected readonly error = signal<string | null>(null);
+
+  loadTransactions(): void {
+    this.loading.set(true);
+    this.error.set(null);
+
+    this.transactionService
+      .getTransactions()
+      .pipe(finalize(() => this.loading.set(false)))
+      .subscribe({
+        next: (transactions) => this.data.set(transactions),
+        error: () => this.error.set('Impossibile caricare le transazioni.'),
+      });
+  }
+
+  ngOnInit(): void {
+    this.loadTransactions();
+  }
+}
